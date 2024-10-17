1566c86e605f836f06b9b63868682731658202c5
51mj <151241834+51mj@users.noreply.github.com>
2024-10-17 09:03:33 -0500

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -15,11 +15,11 @@ class VideoHomepage:
         title_frame.grid(row=0, column=0, columnspan=3, sticky="ew")
         title_frame.grid_propagate(False)
                 
-        title_label = tk.Label(title_frame, text="Video Store Homepage", bg="gray", font=("Arial", 24))
+        title_label = tk.Label(title_frame, text="Video Store Homepage", bg="gray", font=("Noteworthy", 36))
         title_label.pack(pady=5)
-
         
-
+        
+         
         search_frame = tk.Frame(self.root)
         search_frame.grid(row=2, column=0, pady=10, sticky="ew") 
  
@@ -31,36 +31,36 @@ class VideoHomepage:
  
         search_button = tk.Button(search_frame, text="Search", command=self.search, font=("Noteworthy", 18))
         search_button.pack(side=tk.LEFT, padx=(5, 0))
-
+         
         
         
         self.root.grid_columnconfigure(0, weight=1)  # Column 1
         self.root.grid_columnconfigure(1, weight=0)  # Column for the line
         self.root.grid_columnconfigure(2, weight=1)  # Column 2
         
-        l1 = tk.Label(self.root, text="Video Inventory", anchor='n')
+        l1 = tk.Label(self.root, text="Video Inventory", anchor='n', font=("Noteworthy", 24))
         l1.grid(row=1, column=0, sticky=tk.NSEW, padx=10, pady=(10, 0))
         
         line = tk.Frame(self.root, width=2, bg="black")
         line.grid(row=1, column=1, rowspan=4, sticky=tk.NS)
         
-        l2 = tk.Label(self.root, text="Customer Information", anchor='n')
+        l2 = tk.Label(self.root, text="Customer Information", anchor='n', font=("Noteworthy", 24))
         l2.grid(row=1, column=2, sticky=tk.NSEW, padx=10, pady=(10, 0))
         
-        self.root.grid_rowconfigure(1, weight=1)
-        
+        self.root.grid_rowconfigure(1, weight=1) 
+        self.root.grid_rowconfigure(2, weight=1)        
         #column 1 buttons
         
-        b1 = tk.Button(self.root, text= "+ Add Videos", height=3, command=self.open_addvid)
-        b1.grid(row=2, column=0, sticky='ew', padx=5, pady=5)
+        b1 = tk.Button(self.root, text= "+ Add Videos", height=5, command=self.open_addvid, font=('Noteworthy', 18))
+        b1.grid(row=3, column=0, sticky='ew', padx=5, pady=5)
         
         #column 2 buttons
         
-        b3 = tk.Button(self.root, text= "+ Add Customer", height=3, command=self.open_addcus)
-        b3.grid(row=2, column=2, sticky='ew', padx=5, pady=5)
-
+        b3 = tk.Button(self.root, text= "+ Add Customer", height=5, command=self.open_addcus, font=('Noteworthy', 18))
+        b3.grid(row=3, column=2, sticky='ew', padx=5, pady=5)
+        
         #button functionality
-
+        
     def search(self):
         if self.search['text'] == "Search":
             search_term = self.search.get().strip()
@@ -80,15 +80,16 @@ class VideoHomepage:
                 
             self.search.config(text="Search")
             self.search.delete(0, tk.END)
-
+   
+   
     def open_addcus(self):
         new_window = tk.Toplevel(self.root)
         CustomerManagementApp(new_window)
-
+        
+        
     def open_addvid(self):
         new_window = tk.Toplevel(self.root)
-        VideoPage(new_window)
-
+        VideoPage(new_window)  
 
 if __name__ == "__main__":
     root = tk.Tk()
